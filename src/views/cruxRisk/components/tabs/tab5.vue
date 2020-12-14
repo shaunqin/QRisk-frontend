cffc422394bff891bb426646e2a20dca61fb3612
liuzuwu9420 <liuzuwu9420@icloud.com>
2020-12-14 10:23:29 +0800

    将专项风险已办恢复上一版本

--- src/views/cruxRisk/components/tabs/tab5.vue
+++ src/views/cruxRisk/components/tabs/tab5.vue
@@ -14,28 +14,30 @@
           <el-tag type="success">{{renderType(row)}}</el-tag>
         </template>
       </el-table-column>
-      <el-table-column prop="no" label="编号" width="140" />
-      <!-- <el-table-column prop="name" label="流程状态" width="180">
+      <!-- <el-table-column prop="no" label="编号" width="140" /> -->
+      <el-table-column prop="name" label="流程状态" width="180">
         <template slot-scope="{row}">
           <el-tag type="warning">{{row.name}}</el-tag>
         </template>
-      </el-table-column> -->
-      <el-table-column prop="title" label="任务标题" show-overflow-tooltip />
-      <el-table-column prop="noteContent" label="通知内容" min-width="150" show-overflow-tooltip />
-      <el-table-column prop="informant" label="填报人" width="130" />
+      </el-table-column>
+      <el-table-column prop="businessTitle" label="任务标题" show-overflow-tooltip />
+      <!-- <el-table-column prop="noteContent" label="通知内容" min-width="150" show-overflow-tooltip /> -->
+      <!-- <el-table-column prop="informant" label="填报人" width="130" />
       <el-table-column label="填报时间" width="140">
         <template slot-scope="{row}">
           {{format(row.informantTime)}}
         </template>
-      </el-table-column>
-      <el-table-column label="发起人" width="130">
+      </el-table-column> -->
+      <el-table-column prop="createBy" label="发起人" width="130" />
+      <el-table-column prop="createTime" label="发起时间" width="140" />
+      <!-- <el-table-column label="发起人" width="130">
         <template slot-scope="{row}">{{row.issueName}}[{{row.staffno}}]</template>
       </el-table-column>
       <el-table-column prop="createDate" label="发起时间" width="140">
         <template slot-scope="{row}">
           {{format(row.createDate)}}
         </template>
-      </el-table-column>
+      </el-table-column> -->
       <el-table-column label width="100" fixed="right">
         <template slot-scope="{row}">
           <el-button type="primary" size="mini" @click="detail(row)">查看详情</el-button>
@@ -89,7 +91,7 @@ export default {
       this.$emit("selectionChange", { selections: selections });
     },
     detail(row) {
-      /* specialRiskGasDoneDetail(row.taskId, row.formId).then(res => {
+      specialRiskGasDoneDetail(row.taskId, row.formId).then(res => {
         if (res.code != '200') {
           this.$message.error(res.msg);
         } else {
@@ -97,8 +99,8 @@ export default {
           _this.data = res.obj;
           _this.dialog = true;
         }
-      }) */
-      specialRiskDetail(row.id).then((res) => {
+      })
+      /* specialRiskDetail(row.id).then((res) => {
         if (res.code != "200") {
           this.$message.error(res.msg);
         } else {
@@ -108,7 +110,7 @@ export default {
           _this.assessmentType = this.assessmentType
           _this.dialog = true;
         }
-      });
+      }); */
     },
     renderType(row) {
       let type = "";
